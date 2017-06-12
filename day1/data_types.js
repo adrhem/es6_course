c4ef2d3cbed74bbea6c7aafc6d996cf8d0e1b477
Gil <gil@roosterinteractive.mx>
2017-06-12 15:24:11 -0500

    day 1, data_types

---
+++ day1/data_types.js
@@ -0,0 +1,17 @@
+var integer = 10;
+var str = "Hello World";
+var double = 10.1;
+var boolean = false;
+var arr = [10,12,13,'Hello',10.6,true];
+var obj = {
+    integer: integer,
+    str: str,
+    double: double,
+    boolean: boolean,
+    arr: arr,
+    obk: {},
+};
+
+console.log(integer, str, double, boolean);
+console.log(arr);
+console.log(obj);
