472de0ee777ba77df23d2f8bbde987e85487ba2d
Gil <gil@roosterinteractive.mx>
2017-06-15 17:55:33 -0500

    port fix

--- day4/server.js
+++ day4/server.js
@@ -3,7 +3,7 @@ const Hapi = require('hapi');
 const routes = require('./routes.js');
 
 const server = new Hapi.Server();
-server.connection({ port: process.env.PORT, host: 'localhost' });
+server.connection({ port: 3000, host: 'localhost' });
 
 server.route(routes.home);
 server.route(routes.hello);
